fe46a1bfa2c01ce5d3bd463a927fe7c1d2cc2c55
Shihab sharar <shihab.ajit@gmail.com>
2016-11-27 02:41:27 +0600

    Error page setup

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -45,6 +45,12 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
-        return parent::render($request, $e);
+         //return parent::render($request, $e);
+        if($e instanceof NotFoundHttpException)
+        {
+            return response()->view('errors.404', [], 404);
+        }
+     //  return parent::render($request, $e);
+        return response()->view('errors.404', [], 404);
     }
 }
